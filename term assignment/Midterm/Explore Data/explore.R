bccf943ae6f80958e0f25bdc9377d433cf2122f2
Tus <54474594+tuskingcup@users.noreply.github.com>
2021-10-23 00:04:02 +0700

    Update explore.R

--- term assignment/Midterm/Explore Data/explore.R
+++ term assignment/Midterm/Explore Data/explore.R
@@ -3,3 +3,5 @@ library(dplyr)
 ggp <- read.csv("https://raw.githubusercontent.com/sit-2021-int214/023-Google_Play_Store_Apps/main/term%20assignment/Midterm/Original%20Data/Google_Play_Store_Apps_Original.csv")
 
 glimpse(ggp)
+
+ggp %>% filter(App=="ROBLOX")
